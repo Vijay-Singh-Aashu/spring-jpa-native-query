616f7933d9d7c64c9ee10d7ac62a72f0a8166b01
Vijay Singh <vijaysinghaashu@gmail.com>
2023-12-08 10:53:02 +0530

    initial commit

---
+++ src/main/java/com/excellence_spring_jpa_native_query/SpringJpaNativeQueryApplication.java
@@ -0,0 +1,13 @@
+package com.excellence_spring_jpa_native_query;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaNativeQueryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJpaNativeQueryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/excellence_spring_jpa_native_query/SpringJpaNativeQueryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.excellence_spring_jpa_native_query;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaNativeQueryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
